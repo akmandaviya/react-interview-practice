4a55533811d9343aa42262e7b0f97a6266c1fa26
akmandaviya <abdulmandaviya@gmail.com>
2025-07-25 19:52:54 +0530

    added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,15 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const HomePage = () => {
   const [name, setName] = useState('')
+  const [count, setCount]= useState(0)
 
   const inputRef = useRef()
+
+  useEffect(() => { 
+    console.log('component mounted');
+    return (() => {console.log('component unmounted')})
+  },[])
 
   const getDataByContolled = (e) => { 
     setName(e.target.value)
@@ -14,17 +20,26 @@ const HomePage = () => {
     console.log(inputRef.current.value)
   }
 
+  const incrementCount = () => { 
+    setCount(count+1)
+  }
+
   return (
     <div>
         {/* controlled components */}
       <div>
         <input type='text' onChange={getDataByContolled}/>
       </div>
-      
+
       {/* uncontrolled components */}
       <div>
         <input type='text' ref={inputRef}/>
         <button onClick={getDataByUnContolled}>Search</button>
+      </div>
+        {/* counter with usestate */}
+      <div>
+        <button onClick={incrementCount}>Increment</button>
+        <p>Count is:{count} </p>
       </div>
     </div>
   )
