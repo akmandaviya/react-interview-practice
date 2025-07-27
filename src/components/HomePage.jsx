d37f2313feec7957e4315766be2f0c4f25d24bc5
akmandaviya <abdulmandaviya@gmail.com>
2025-07-27 13:01:08 +0530

    new added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,47 +1,105 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react'
 
 const HomePage = () => {
   const [name, setName] = useState('')
-  const [count, setCount]= useState(0)
+  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
 
   const inputRef = useRef()
 
-  useEffect(() => { 
+  useEffect(() => {
     console.log('component mounted');
-    return (() => {console.log('component unmounted')})
-  },[])
+    return (() => { console.log('component unmounted') })
+  }, [])
 
-  const getDataByContolled = (e) => { 
+  const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+  ]
+
+  const filteredData = useMemo(() => {
+    return users.filter(item => item.name.toLowerCase() === search.toLowerCase())
+  }, [search])
+
+  const getDataByContolled = (e) => {
     setName(e.target.value)
     console.log(e.target.value)
   }
 
-  const getDataByUnContolled = () => { 
+  const getDataByUnContolled = () => {
     console.log(inputRef.current.value)
   }
 
-  const incrementCount = () => { 
-    setCount(count+1)
+  const incrementCount = () => {
+    setCount(count + 1)
   }
+
+  const intialState = {count:0}
+
+  const reducerFunc = (state, action) => { 
+    switch(action.type) { 
+      case 'increment':
+        return {count: state.count + 1}
+      case 'decrement':
+        return {count: state.count - 1}
+      case 'reset':
+        return intialState
+      default:
+        state
+    }
+  }
+
+  const btnIncrement = useCallback(() => { 
+    setCount( prev => prev + 1 )
+  }, [])
+
+  const [state, dispatch] = useReducer(reducerFunc, intialState)
 
   return (
     <div>
-        {/* controlled components */}
+      {/* controlled components */}
       <div>
-        <input type='text' onChange={getDataByContolled}/>
+        <input type='text' onChange={getDataByContolled} />
       </div>
 
       {/* uncontrolled components */}
       <div>
-        <input type='text' ref={inputRef}/>
+        <input type='text' ref={inputRef} />
         <button onClick={getDataByUnContolled}>Search</button>
       </div>
-        {/* counter with usestate */}
+
+      {/* counter with usestate */}
       <div>
         <button onClick={incrementCount}>Increment</button>
         <p>Count is:{count} </p>
       </div>
+
+      {/* filter user data using use memo */}
+      <div>
+        <input
+          value={search}
+          type='text'
+          onChange={(e) => setSearch(e.target.value)} />
+        <ul>
+          {filteredData.map(user => <li key={user.id}>{user.name}</li>)}
+        </ul>
+      </div>
+
+      {/* count using use reducer */}
+      <div>
+        <h2>Count : {state.count}</h2>
+        <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
+        <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+        <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+      </div>
+
+      <div>
+         Memoised Counter: { count }
+        <button onClick={btnIncrement}>Btn Counter</button>
+      </div>
     </div>
+
   )
 }
 
